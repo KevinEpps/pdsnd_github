493852fc3ac5b986851984ae0a50f511def7a35c
Kevin Epps <kevinepps06@gmail.com>
2019-02-03 11:21:21 +0300

    Udated comments for global variable code

--- Bikeshare_Final.py
+++ Bikeshare_Final.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import calendar
 
-
+#Define global variables
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
